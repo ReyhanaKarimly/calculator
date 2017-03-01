cdaaf4d1df4b633af9458ae0c3961ce759ae4be0
ReyhanaKarimly <karimlyreyhana@gmail.com>
2017-03-01 23:10:20 +0400

    my_first_calculator

---
+++ calculator/calculator.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int main()
+{
+	float a,b;
+	char mode;
+	printf("Enter the first number: ");
+	scanf("%f", &a);
+	printf("Enter the operator: ");
+	scanf("\n%c", &mode);
+	printf("Enter the second number: ");
+	scanf ("%f", &b);
+		if (mode=='+')
+		{
+			printf("\nsum=%.2f+%.2f=%.2f", a,b,a+b);
+		}
+		else if (mode=='-')
+		{
+			printf("\ndifference=%.f-%.f=%.f", a,b,a-b);
+		}
+		else if(mode=='*')
+		{
+			printf("\nproduct=%.2f*%.2f=%.2f", a,b,a*b);
+		}
+		else if (mode=='/' && b!=0)
+		{
+		printf("\nratio=%.3f/%.3f=%.3f",a,b,a/b);
+		}
+		else if (mode=='/' && b==0)
+		{
+		printf("ERROR! 'a/0'");
+		}
+		else
+printf("Wrong operator!");
+		getch();
+		return 0;
+		
+}
